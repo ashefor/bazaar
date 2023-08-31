2718e3bbb914093a0973ecbf986afd4c07bb1841
ashefor <mashefor@gmail.com>
2023-08-31 20:11:32 +0100

    fix: change baseUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://bazaar-api.cptshredder.com',
-  baseUrl: 'http://localhost:8000',
+  baseUrl: 'https://bazaar-api.cptshredder.com',
 };
