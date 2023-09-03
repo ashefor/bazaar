16f66c6b8a7d1d8fbf120b112c9f47a970e33fca
ashefor <mashefor@gmail.com>
2023-09-03 18:50:54 +0100

    chore: change baseUrl back to vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:8000',
-  // baseUrl: 'https://bazaar-api.cptshredder.com',
-  baseUrl: 'https://bazaar-api.onrender.com'
+  baseUrl: 'https://bazaar-api.cptshredder.com',
+  // baseUrl: 'https://bazaar-api.onrender.com'
 };
